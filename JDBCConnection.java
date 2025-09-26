8c8b020810d3acd61c8a900558568afaad56c656
PARISIBOYINA VIJAY VARDHAN <143518938+pvijay63@users.noreply.github.com>
2025-09-26 22:01:27 +0530

    Add files via upload
    Building JDBC connection (Building connection between java and mysql )

---
+++ JDBCConnection.java
@@ -0,0 +1,18 @@
+package com.shopping.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBCConnection {
+
+	 public static Connection forMySQLConnection() throws ClassNotFoundException, SQLException 
+	 {
+	
+		 Class.forName("com.mysql.cj.jdbc.Driver");
+		 
+		 return DriverManager.getConnection("jdbc:mysql://localhost:3306/a14_e_commerce?user=root&password=123456789");
+		 
+	 }
+	
+}
